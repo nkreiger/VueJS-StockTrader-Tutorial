50185ac6e2d3470ca4d7bf51678bb983404f321b
Noah Kreiger <noahkreiger@Noahs-MacBook-Pro.local>
2020-01-28 22:43:43 -0500

    set up sandbox

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
+// import external vuejs helpers
+import VueLogger from 'vuejs-logger';
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+// import main app
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// import bootstrap css files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// set production
+const isProduction = process.env.NODE_ENV === 'production';
+
+// create logging
+Vue.config.productionTip = isProduction
+const options = {
+  isEnabled: true,
+  logLevel: isProduction ? 'error' : 'debug',
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: true,
+  separator: '|',
+  showConsoleColors: true
+};
+
+// enabkle services
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueLogger, options);
+
+// create and mount vue spa app on index.html
 new Vue({
   render: h => h(App),
 }).$mount('#app')
